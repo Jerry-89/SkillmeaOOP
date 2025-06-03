2e976d8a2626471b8103b3b6952025c929aa9173
Jerry-89 <kontakt.jerry@gmail.com>
2025-06-03 20:31:33 +0200

    druhy comit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
+import java.awt.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    RodinnyDom domJanik = new RodinnyDom();
+    domJanik.pocetPoschodi = 4;
+    domJanik.rozloha = 457f; // f na konci lebo je to float
+    domJanik.material = "drevotrieska";
+    domJanik.pocOkien = 3;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
-}
+    RodinnyDom domZajac = new RodinnyDom(); //vytvoril som dalsi rodinny dom
+    domZajac.pocetPoschodi = 5;
+    domZajac.rozloha = 457f;
+    domZajac.material = "tehla";
+    domZajac.pocOkien = 5;
---
+++ src/RodinnyDom.java
@@ -0,0 +1,6 @@
+public class RodinnyDom {
+    int pocetPoschodi;
+    String material; //string, pretoze to bude text
+    float rozloha; //float pretoze to moze byt desatinne cislo
+    int pocOkien;
+}
